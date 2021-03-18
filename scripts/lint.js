da2eb92af8799d4742ccf46e6c41da981138a8ba
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-18 00:34:58 -0300

    test(lint): add linter script

---
+++ scripts/lint.js
@@ -0,0 +1,5 @@
+const shelljs = require('shelljs');
+
+const result = shelljs.exec('git diff --name-only main..main', { silent: true });
+
+console.log(result.stdout.split('\n').filter((data) => Boolean(data)));
