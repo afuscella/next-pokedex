ddbcd2cab29c39706c832972b97be42341976755
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-26 12:06:48 -0300

    ci(script): adjust diff script to point master destination branch instead

--- scripts/diff.js
+++ scripts/diff.js
@@ -4,6 +4,6 @@ const shelljs = require('shelljs');
 const currentBranch = shelljs.exec('git branch --show-current', { silent: true }).stdout;
 console.log('starting analysis from branch:', currentBranch);
 
-const result = shelljs.exec(`git diff --name-only ${currentBranch}..main`, { silent: true });
+const result = shelljs.exec(`git diff --name-only ${currentBranch}..master`, { silent: true });
 
 console.log(result.stdout.split('\n').filter((data) => Boolean(data)));
