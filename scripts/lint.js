6c0dc51441a77bac4e1a7c7e44f86ff5c5c4812b
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-26 11:52:47 -0300

    fix(ci): adjust custom-script test to compare current branch with master

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-console */
 const shelljs = require('shelljs');
 
-const result = shelljs.exec('git diff --name-only main..main', { silent: true });
+const currentBranch = shelljs.exec('git branch --show-current', { silent: true });
+console.log('starting analysis from branch:', currentBranch);
+
+const result = shelljs.exec(`git diff --name-only ${currentBranch}..main`, { silent: true });
 
 console.log(result.stdout.split('\n').filter((data) => Boolean(data)));
